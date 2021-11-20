628bc1998c25a5fa49c0a9af7c1581ea90eb9ed8
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-11-20 12:22:13 -0600

    update;

---
+++ pages/components/Products.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/Products.module.css';
+
+
+const Products = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default Products
