4d68d0830cbdb979e854a405147e6a559fe9d1fc
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-11-20 09:48:41 -0600

    update-nav

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import styles from '../../styles/Navbar.module.css'
-
+import Link from 'next/link'
 const NavBar = () => {
   return (
     <div className={styles.container}>
-      NavBar
+      <Link href='/'>LMJ</Link>
+      <ul className={styles.list}>
+          <li className={styles.listItem}>
+            <Link href='/products/aretes'>Aretes</Link>
+         </li>
+         <li className={styles.listItem}>
+             <Link href='/products/anillos'>Anillos</Link>
+             </li>
+             <li className={styles.listItem}>
+             <Link href='/products/Aritos'>Aretes</Link>
+             </li>
+             <li className={styles.listItem}>
+             <Link href='/products/cadenas'>Anillos</Link>
+             </li>
+             <li className={styles.listItem}>
+             <Link href='/products/Contact'>Contact</Link>
+             </li>
+
+      </ul>
     </div>
   )
 }
