314770fa882babb334f1059e1a2278da0cb21a89
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-11-20 09:29:30 -0600

    update-nav-bar

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from '../../styles/Navbar.module.css'
 
 const NavBar = () => {
   return (
-    <div>
+    <div className={styles.container}>
       NavBar
     </div>
   )
